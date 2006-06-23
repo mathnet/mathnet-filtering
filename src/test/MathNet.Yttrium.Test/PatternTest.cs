3d2c4681f40406f0ffd73b4c56a47ba01351be35
Christoph Ruegg <git@cdrnet.ch>
2006-06-23 12:46:21 +0000

    test whether failed NUnit tests really are noticed by CCNet

---
+++ src/test/MathNet.Yttrium.Test/PatternTest.cs
@@ -0,0 +1,22 @@
+﻿//using Microsoft.VisualStudio.QualityTools.UnitTesting.Framework;
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+using MathNet.Symbolics.Workplace;
+using MathNet.Symbolics.Core;
+using MathNet.Symbolics.Backend;
+
+namespace Yttrium.UnitTests
+{
+    [TestFixture]
+    public class PatternTest
+    {
+        [Test]
+        public void Pattern_TestFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
