62e9a5f420adce1c647c52bc9703069cb748c1e3
Christoph Ruegg <git@cdrnet.ch>
2006-08-06 17:17:55 +0000

    new constants definitions: math & science

---
+++ src/app/MathNet.Iridium/Library/Constants.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathNet.Numerics
+{
+    /// <summary>
+    /// Mathematical Constants
+    /// </summary>
+    /// <seealso cref="SiConstants"/>
+    public static class Constants
+    {
+        /// <summary>e</summary>
+        public const double E = 2.7182818284590452353602874713526624977572470937000d;
+        /// <summary>log[2](e)</summary>
+        public const double Log2E = 1.4426950408889634073599246810018921374266459541530d;
+        /// <summary>log[10](e)</summary>
+        public const double Log10E = 0.43429448190325182765112891891660508229439700580366d;
+        /// <summary>log[e](2)</summary>
+        public const double Ln2 = 0.69314718055994530941723212145817656807550013436026d;
+        /// <summary>log[e](10)</summary>
+        public const double Ln10 = 2.3025850929940456840179914546843642076011014886288d;
+        /// <summary>log[e](pi)</summary>
+        public const double LnPi = 1.1447298858494001741434273513530587116472948129153d;
+
+        /// <summary>1/e</summary>
+        public const double InvE = 0.36787944117144232159552377016146086744581113103176d;
+        /// <summary>sqrt(e)</summary>
+        public const double SqrtE = 1.6487212707001281468486507878141635716537761007101d;
+
+        /// <summary>sqrt(2)</summary>
+        public const double Sqrt2 = 1.4142135623730950488016887242096980785696718753769d;
+        /// <summary>sqrt(1/2)</summary>
+        public const double Sqrt1_2 = 0.70710678118654752440084436210484903928483593768845d;
+        /// <summary>sqrt(3)/2</summary>
+        public const double HalfSqrt3 = 0.86602540378443864676372317075293618347140262690520d;
+
+        /// <summary>pi</summary>
+        public const double Pi = 3.1415926535897932384626433832795028841971693993751d;
+        /// <summary>pi/2</summary>
+        public const double Pi_2 = 1.5707963267948966192313216916397514420985846996876d;
+        /// <summary>pi/4</summary>
+        public const double Pi_4 = 0.78539816339744830961566084581987572104929234984378d;
+        /// <summary>sqrt(pi)</summary>
+        public const double SqrtPi = 1.7724538509055160272981674833411451827975494561224d;
+        /// <summary>sqrt(2pi)</summary>
+        public const double Sqrt2Pi = 2.5066282746310005024157652848110452530069867406099d;
+
+        /// <summary>1/pi</summary>
+        public const double InvPi = 0.31830988618379067153776752674502872406891929148091d;
+        /// <summary>2/pi</summary>
+        public const double TwoInvPi = 0.63661977236758134307553505349005744813783858296182d;
+        /// <summary>1/sqrt(pi)</summary>
+        public const double InvSqrtPi = 0.56418958354775628694807945156077258584405062932899d;
+        /// <summary>1/sqrt(2pi)</summary>
+        public const double InvSqrt2Pi = 0.39894228040143267793994605993438186847585863116492d;
+        /// <summary>2/sqrt(pi)</summary>
+        public const double TwoInvSqrtPi = 1.1283791670955125738961589031215451716881012586580d;
+    }
+}
---
+++ src/app/MathNet.Iridium/Library/SiConstants.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathNet.Numerics
+{
+    /// <summary>
+    /// Science Constants
+    /// </summary>
+    /// <seealso cref="Constants"/>
+    public static class SiConstants
+    {
+        #region SI Prefixes
+        /// <summary>1 000 000 000 000 000 000 000 000</summary>
+        public const double yotta = 1e24;
+        /// <summary>1 000 000 000 000 000 000 000</summary>
+        public const double zetta = 1e21;
+        /// <summary>1 000 000 000 000 000 000</summary>
+        public const double exa = 1e18;
+        /// <summary>1 000 000 000 000 000</summary>
+        public const double peta = 1e15;
+        /// <summary>1 000 000 000 000</summary>
+        public const double tera = 1e12;
+        /// <summary>1 000 000 000</summary>
+        public const double giga = 1e9;
+        /// <summary>1 000 000</summary>
+        public const double mega = 1e6;
+        /// <summary>1 000</summary>
+        public const double kilo = 1e3;
+        /// <summary>100</summary>
+        public const double hecto = 1e2;
+        /// <summary>10</summary>
+        public const double deca = 1e1;
+        /// <summary>0.1</summary>
+        public const double deci = 1e-1;
+        /// <summary>0.01</summary>
+        public const double centi = 1e-2;
+        /// <summary>0.001</summary>
+        public const double milli = 1e-3;
+        /// <summary>0.000 001</summary>
+        public const double micro = 1e-6;
+        /// <summary>0.000 000 001</summary>
+        public const double nano = 1e-9;
+        /// <summary>0.000 000 000 001</summary>
+        public const double pico = 1e-12;
+        /// <summary>0.000 000 000 000 001</summary>
+        public const double femto = 1e-15;
+        /// <summary>0.000 000 000 000 000 001</summary>
+        public const double atto = 1e-18;
+        /// <summary>0.000 000 000 000 000 000 001</summary>
+        public const double zepto = 1e-21;
+        /// <summary>0.000 000 000 000 000 000 000 001</summary>
+        public const double yocto = 1e-24;
+        #endregion
+
+        #region Physical Constants
+        /// <summary>Speed of Light in Vacuum: c_0 = 2.99792458e8 [m s^-1] (defined, exact; 2002 CODATA)</summary>
+        public const double SpeedOfLight = 2.99792458e8;
+        /// <summary>Magnetic Permeability in Vacuum: mu_0 = 4*Pi * 10^-7 [N A^-2 = kg m A^-2 s^-2] (defined, exact; 2002 CODATA)</summary>
+        public const double MagneticPermeability = 1.2566370614359172953850573533118011536788677597500e-6;
+        /// <summary>Electric Permittivity in Vacuum: epsilon_0 = 1/(mu_0*c_0^2) [F m^-1 = A^2 s^4 kg^-1 m^-3] (defined, exact; 2002 CODATA)</summary>
+        public const double ElectricPermittivity = 8.8541878171937079244693661186959426889222899381429e-12;
+        /// <summary>Characteristic Impedance of Vacuum: Z_0 = mu_0*c_0 [Ohm = m^2 kg s^-3 A^-2] (defined, exact; 2002 CODATA)</summary>
+        public const double CharacteristicImpedanceVacuum = 376.73031346177065546819840042031930826862350835242;
+
+        /// <summary>Newtonian Constant of Gravitation: G = 6.6742e-11 [m^3 kg^-1 s^-2] (2002 CODATA)</summary>
+        public const double GravitationalConstant = 6.6742e-11;
+        /// <summary>Planck's constant: h = 6.6260693e-34 [J s = m^2 kg s^-1] (2002 CODATA)</summary>
+        public const double PlancksConstant = 6.6260693e-34;
+        /// <summary>Reduced Planck's constant: h_bar = h / (2*Pi) [J s = m^2 kg s^-1] (2002 CODATA)</summary>
+        public const double DiracsConstant = 1.05457168e-34;
+        /// <summary>Planck mass: m_p = (h_bar*c_0/G)^(1/2) [kg] (2002 CODATA)</summary>
+        public const double PlancksMass = 2.17645e-8;
+        /// <summary>Planck temperature: T_p = (h_bar*c_0^5/G)^(1/2)/k [K] (2002 CODATA)</summary>
+        public const double PlancksTemperature = 1.41679e32;
+        /// <summary>Planck length: l_p = h_bar/(m_p*c_0) [m] (2002 CODATA)</summary>
+        public const double PlancksLength = 1.61624e-35;
+        /// <summary>Planck time: t_p = l_p/c_0 [s] (2002 CODATA)</summary>
+        public const double PlancksTime = 5.39121e-44;
+
+        /// <summary>Elementary Electron Charge: e = 1.60217653e-19 [C = A s] (2002 CODATA)</summary>
+        public const double ElementaryCharge = 1.60217653e-19;
+        /// <summary>Magnetic Flux Quantum: theta_0 = h/(2*e) [Wb = m^2 kg s^-2 A^-1] (2002 CODATA)</summary>
+        public const double MagneticFluxQuantum = 2.06783372e-15;
+        /// <summary>Conductance Quantum: G_0 = 2*e^2/h [S = m^-2 kg^-1 s^3 A^2] (2002 CODATA)</summary>
+        public const double ConductanceQuantum = 7.748091733e-5;
+        
+        
+        public const double JosephsonConstant = 483597.879e9;
+        public const double VonKlitzingConstant = 25812.807449;
+        public const double BohrMagneton = 927.400949e-26;
+        public const double NuclearMagneton = 5.05078343e-27;
+        public const double FineStructureConstant = 7.297352568e-3;
+        public const double RydbergConstant = 10973731.568525;
+        public const double BohrRadius = 0.5291772108e-10;
+        public const double HartreeEnergy = 4.35974417e-18;
+        public const double QuantumOfCirculation = 3.636947550e-4;
+        public const double FermiCouplingConstant = 1.16639e-5;
+        public const double WeakMixingAngle = 0.22215;
+
+        public const double ElectronMass = 9.1093826e-31;
+        public const double ElectronMassEnergyEquivalent = 8.1871047e-14;
+        public const double ElectronMolarMass = 5.4857990945e-7;
+        public const double ComptonWavelength = 2.426310238e-12;
+        //public const double ComptonWavelengthOver2Pi = 386.1592678e-15;
+        public const double ClassicalElectronRadius = 2.817940325e-15;
+        public const double ThomsonCrossSection = 0.665245873e-28;
+        public const double ElectronMagneticMoment = -928.476412e-26;
+        public const double ElectronGFactor = -2.0023193043718;
+
+        public const double MuonMass = 1.88353140e-28;
+        public const double MuonMassEnegryEquivalent = 1.69283360e-11;
+        public const double MuonMolarMass = 0.1134289264e-3;
+        public const double MuonComptonWavelength = 11.73444105e-15;
+        public const double MuonMagneticMoment = -4.49044799e-26;
+        public const double MuonGFactor = -2.0023318396;
+
+        public const double TauMass = 3.16777e-27;
+        public const double TauMassEnergyEquivalent = 2.84705e-10;
+        public const double TauMolarMass = 1.90768e-3;
+        public const double TauComptonWavelength = 0.69772e-15;
+
+        public const double ProtonMass = 1.67262171e-27;
+        public const double ProtonMassEnergyEquivalent = 1.50327743e-10;
+        public const double ProtonMolarMass = 1.00727646688e-3;
+        public const double ProtonComptonWavelength = 1.3214098555e-15;
+        public const double ProtonMagneticMoment = 1.41060671e-26;
+        public const double ProtonGFactor = 5.585694701;
+        public const double ShieldedProtonMagneticMoment = 1.41057047e-26;
+        public const double ProtonGyromagneticRatio = 2.67522205e8;
+        public const double ShieldedProtonGyromagneticMoment = 2.67515333e8;
+
+        public const double NeutronMass = 1.67492728e-27;
+        public const double NeutronMassEnegryEquivalent = 1.50534957e-10;
+        public const double NeutronMolarMass = 1.00866491560e-3;
+        public const double NeutronComptonWavelength = 1.3195909067e-1;
+        public const double NeutronMagneticMoment = -0.96623645e-26;
+        public const double NeutronGFactor = -3.82608546;
+        public const double NeutronGyromagneticRatio = 1.83247183e8;
+
+        public const double DeuteronMass = 3.34358335e-27;
+        public const double DeuteronMassEnegryEquivalent = 3.00506285e-10;
+        public const double DeuteronMolarMass = 2.01355321270e-3;
+        public const double DeuteronMagneticMoment = 0.433073482e-26;
+
+        public const double HelionMass = 5.00641214e-27;
+        public const double HelionMassEnegryEquivalent = 4.49953884e-10;
+        public const double HelionMolarMass = 3.0149322434e-3;
+        //public const double  = ;
+        //public const double  = ;
+        //public const double  = ;
+        
+        #endregion
+    }
+}
