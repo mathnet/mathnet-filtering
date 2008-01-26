deb1849b808b767c1f8190eec95d105b7fabb6fa
Christoph Ruegg <git@cdrnet.ch>
2008-01-26 08:08:39 +0000

    removing/moving some non-code files

--- Iridium_Build_DebugOnly.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cls
-
-msbuild Iridium.sln /p:Configuration=Debug /t:Build
-
-pause
--- Iridium_Clean.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-
-msbuild Iridium.sln /p:Configuration=Release /t:Clean
-msbuild Iridium.sln /p:Configuration=Debug /t:Clean
-
-pause
