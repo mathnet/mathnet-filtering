1d6b1a5096b41433c6b00bc0840ce933725cc834
Christoph Ruegg <git@cdrnet.ch>
2014-07-02 14:02:29 +0200

    Relicense parts we can to MIT/X11 (some parts and thus the project itself still LGPL)

--- src/Filtering/Windowing/LanczosWindow.cs
+++ src/Filtering/Windowing/LanczosWindow.cs
@@ -1,23 +1,31 @@
-#region Math.NET Neodym (LGPL) by Christoph Ruegg
-// Math.NET Neodym, part of the Math.NET Project
-// http://mathnet.opensourcedotnet.info
+// <copyright file="LanczosWindow.cs" company="Math.NET">
+// Math.NET Filtering, part of the Math.NET Project
+// http://filtering.mathdotnet.com
+// http://github.com/mathnet/mathnet-filtering
 //
-// Copyright (c) 2001-2008, Christoph Rüegg,  http://christoph.ruegg.name
-//						
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published 
-// by the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// Copyright (c) 2009-2014 Math.NET
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
 //
-// You should have received a copy of the GNU Lesser General Public 
-// License along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-#endregion
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
 
 namespace MathNet.Filtering.Windowing
 {
